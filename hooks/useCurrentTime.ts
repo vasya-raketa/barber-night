783468e9601159b81df61579238c4fe6a22b0549
Vadzim Litvinka <vadzimlitvinka@Vadzims-MacBook-Air.local>
2026-06-20 10:03:30 +0200

    update categories and font

--- hooks/useCurrentTime.ts
+++ hooks/useCurrentTime.ts
@@ -6,6 +6,7 @@ export function useCurrentTime(): Date {
   const [now, setNow] = useState(() => new Date());
 
   useEffect(() => {
+    setNow(new Date());
     const interval = setInterval(() => setNow(new Date()), 1_000);
     return () => clearInterval(interval);
   }, []);
